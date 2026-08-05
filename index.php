9599dfbc5998ac7e157a177d64df7a21298401c4
VITOR AIRES DIOGENES <vitor_diogenes@estudante.sesisenai.org.br>
2026-08-05 09:30:05 -0300

    estrutura básica

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+   
+    <title>Gerenciador de S.A</title>
+</head>
+<body>
+    <aside class = 'sidebar'>
+        <a href="">Grupos</a>
+        <a href="">Projetos</a>
+        <a href="">Feed</a>
+        <a href="" style = "align-self:last baseline;">Conta</a>
+    </aside>
+
+    <script src="assets/script.js"></script>
+</body>
+</html>
