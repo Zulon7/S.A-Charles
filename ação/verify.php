5d6238e46e6b4b7a4052582d7cc36137dced833d
f-k-nas <felipe_nascimento4@estudante.sesisenai.org.br>
2026-08-13 07:33:34 -0300

    ação de verificar sessão

--- ação/verify.php
+++ ação/verify.php
@@ -1,11 +1,5 @@
 <?php
 
-  // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
-  if (!empty($_POST) AND (empty($_POST["usuario"]) OR empty($_POST["senha"]))) {
-      header("Location: index.php"); exit;
-  }
-
-
   $conexao = new mysqli("127.0.0.1", "root", "", "samedia");
 
   if ($conexao->connect_errno) {
@@ -16,17 +10,14 @@
   $senha = mysql_real_escape_string($_POST["senha"]);
 
   // Validação do usuário/senha digitados
-  $sql = "SELECT `id`, `nome`, `nivel`, `senha` FROM `users` WHERE (`nome` = "".$usuario ."") AND (`senha` = "". $senha ."") AND (`ativo` = 1) LIMIT 1";
+  $sql = "SELECT `id`, `senha` FROM `users` WHERE (`id` = "".$_SESSION['id'] ."") AND (`senha` = "". $_SESSION['senha'] ."") AND (`ativo` = 1) LIMIT 1";
   $query = mysql_query($sql);
   if ((mysql_num_rows($query) != 1) || session_status == PHP_SESSION_NONE || PHP_SESSION_DISABLED)  {
       // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
-      echo "Login inválido!"; exit;
+      echo "Login inválido!"; $_SESSION = array(); exit;
   } else {
       // Salva os dados encontados na variável $resultado
-      $resultado = mysql_fetch_assoc($query);
-      session_start();
-      $_SESSION['id'] = $resultado['id'];
-      $_SESSION['senha'] = $resultado['senha'];
+      echo "Sessão verificada";
   }
 
   $conexao -> close();
