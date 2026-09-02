2dcfa46188637dfdffc88bd32e2187608cd579aa
f-k-nas <felipe_nascimento4@estudante.sesisenai.org.br>
2026-09-02 12:02:25 -0300

    stylesheet e site base não funcional

---
+++ estilizado/main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feed de Situação de Aprendizado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="main">
+            <div class="sidebar-esq">
+                <p>Esquerda</p>
+            </div>
+            <div class="central">
+                
+                <div class="header">
+                    <p>Header</p>
+                </div>
+
+                <div class="content">
+                    <p>Conteúdo</p>
+                </div>
+                
+                <div class="footer">
+                    <p>Footer</p>
+                </div>
+            </div>
+
+            <!--
+            <div class="sidebar-dir">
+                <p>Direita</p>
+            </div>
+            -->
+            
+        </div>
+
+        
+    </div>
+</body>
+</html>
